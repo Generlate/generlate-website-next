8183a0ac57a26576f5a79e8a095bf7d00b08c07f
Austen Cabret <austencabret@gmail.com>
2024-03-13 09:47:52 -0400

    fix footer dropdown

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import React, { useState, useEffect } from "react";
 import Link from 'next/link';
 import { TbVector } from "react-icons/tb";
 import { TiDocumentText } from "react-icons/ti";
@@ -6,51 +9,80 @@ import { BsPeople } from "react-icons/bs";
 import styles from "@/app/styles/footer.module.css"
 
 export default function Footer() {
+  const [activeDropdown, setActiveDropdown] = useState(null);
+
+  const toggleDropdown = (dropdownId: any) => {
+    setActiveDropdown(activeDropdown === dropdownId ? null : dropdownId);
+  };
+
+  const handleMouseEnter = (dropdownId: any) => {
+    if (activeDropdown !== dropdownId) {
+      toggleDropdown(dropdownId);
+    }
+  };
+
+  const handleClickOutside = (event: any) => {
+    if (!event.target.closest(".dropdown")) {
+      setActiveDropdown(null);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+
+    // Clean up the event listener
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+
+
   return (
     <footer className={styles.footer}>
-      <div className={`${styles.div} ${styles['div-hover']}`}>
-        <button className={styles.link} data-dropdown-button>
-        <TbVector />
-        <p>Product</p>
+      <div className={`${styles.div} ${styles['div:hover']} ${styles.dropdown} ${activeDropdown === "product" ? `${styles.active}` : ``}`} onMouseEnter={() => handleMouseEnter("product")}>
+        <button className={styles.link} data-dropdown-button onClick={() => toggleDropdown("product")}>
+          <TbVector />
+          <p>Product</p>
         </button>
-        <div className={`${styles["dropdown-menu"]}`}>
+        <div className={styles["dropdown-menu"]}>
           <Link href="/news">News</Link>
           <Link href="/pricing">Pricing</Link>
         </div>
       </div>
 
-      <div >
-        <button className={styles.link} data-dropdown-button>
+      <div className={`${styles.dropdown} ${activeDropdown === "resources" ? `${styles.active}` : ``}`} onMouseEnter={() => handleMouseEnter("resources")}>
+        <button className={styles.link} data-dropdown-button onClick={() => toggleDropdown("resources")}>
           <TiDocumentText />
           <p>Resources</p>
         </button>
-        <div className={`${styles["dropdown-menu"]}`}>
+        <div className={styles["dropdown-menu"]}>
           <Link href="/docs">Docs</Link>
           <Link href="/api">API</Link>
         </div>
       </div>
 
-      <div>
-        <button className={styles.link} data-dropdown-button>
+      <div className={`${styles.dropdown} ${activeDropdown === "company" ? `${styles.active}` : ``}`} onMouseEnter={() => handleMouseEnter("company")}>
+        <button className={styles.link} data-dropdown-button onClick={() => toggleDropdown("company")}>
           <IoBusinessOutline />
           <p>Company</p>
         </button>
-        <div className={`${styles["dropdown-menu"]}`}>
+        <div className={styles["dropdown-menu"]}>
           <Link href="/team">Team</Link>
           <Link href="/finances">Finances</Link>
           <Link href="/legal">Legal</Link>
         </div>
       </div>
 
-      <div>
-        <button className={styles.link}data-dropdown-button>
+      <div className={`${styles.dropdown} ${activeDropdown === "social" ? `${styles.active}` : ``}`} onMouseEnter={() => handleMouseEnter("social")}>
+        <button className={styles.link} data-dropdown-button onClick={() => toggleDropdown("social")}>
           <BsPeople />
           <p>Social</p>
         </button>
-        <div className={`${styles["dropdown-menu"]}`}>
-          <Link href="https://github.com/Generlate">Github</Link>,
-          <Link href="https://www.youtube.com/@generlate">Youtube</Link>,
-          <Link href="https://twitter.com/Generlate">Twitter</Link>,
+        <div className={styles["dropdown-menu"]}>
+          <Link href="https://github.com/Generlate">Github</Link>
+          <Link href="https://www.youtube.com/@generlate">Youtube</Link>
+          <Link href="https://twitter.com/Generlate">Twitter</Link>
           <Link href="https://www.linkedin.com/company/78367423/admin/feed/posts/">LinkedIn</Link>
         </div>
       </div>
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import Image from 'next/image';
 import Link from 'next/link';
 import { BiUserCircle } from "react-icons/bi";
@@ -8,20 +9,61 @@ import { RiUserFollowFill } from "react-icons/ri";
 import styles from "@/app/styles/header.module.css"
 
 export default function Header() {
+  let profile: React.ReactNode =  (
+    <BiUserCircle size={34} title="user options" />
+  );
+
+  let menu;
+
+  if (0) {
+    menu = (
+      <ul>
+        <li>
+          <RiUserFollowFill size={20} />
+          <Link href="/components/login">Sign in</Link>
+        </li>
+        <li>
+          <TiUserAdd size={23} />
+          <Link href="/components/signup">Sign up</Link>
+        </li>
+      </ul>
+    );
+  } else {
+    menu = (
+      <ul>
+        <button className={styles.link}>
+          <VscColorMode />
+          <p>theme</p>
+        </button>
+
+        <div>
+          <BiUserCircle size={19} />
+          <input />
+        </div>
+
+        <li>
+          <ImExit size={19} />
+          <Link href="/components/login">Log out</Link>
+        </li>
+      </ul>
+    );
+  }
+
+
   return (
     <header className={styles.header}>
       <Link href="/about">
-        <Image src="/generlate-light.webp" width={250} height={47} alt="picture of cubes" className={`${styles['img']} ${styles['img-hover']}`}/>
+        <Image src="/generlate-light.webp" width={250} height={47} alt="picture of cubes" className={`${styles['img']} ${styles['img:hover']}`}/>
       </Link>
-      <div>
-        <button>
+      <div className={styles.dropdown}>
+        <button className={styles.link} data-dropdown-button>
           <BiUserCircle size={34} title="user options" />
           <VscColorMode />
           <RiUserFollowFill size={20} />
           <TiUserAdd size={23} />
           <ImExit size={19} />
         </button>
-        <form>
+        <form className={`${styles["dropdown-menu"]}`}>
           <div></div>
           <div></div>
         </form>
