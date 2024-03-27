9b9f6b54e33117077024d1d234fb45db93e326ba
Austen Cabret <austencabret@gmail.com>
2024-03-27 17:41:07 -0400

    try making get happen after put

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -23,11 +23,6 @@ function Home(){
   }
 
   function handleInputClick() {
-    // get user input text 
-    // post user input data to the server
-    // server will return a file path to the model file
-    // update canvas with the model file
-
     const input = document.getElementById("generationbar");
 
     if (input instanceof HTMLInputElement) {
@@ -40,40 +35,34 @@ function Home(){
         body: formData,
         credentials: "include"
       })
-        .then((response) => response.json())
-        .catch((error) => {
-          console.error("Error uploading user input text:", error);
-        });
-
-        
-      fetch("https://api.generlate.com/api/user-data", {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-        credentials: "include"
-      })
-        .then((response) => response.json())
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("PUT request failed");
+          }
+          return response.json(); // Parse the response body as JSON
+        })
+        .then(() => {
+          // After successful PUT request, make the GET request
+          return fetch("https://api.generlate.com/api/user-data", {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+            credentials: "include"
+          });
+        })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("GET request failed");
+          }
+          return response.json(); // Parse the response body as JSON
+        })
         .then((data) => {
-          // const generatedObjectPath = "https://api.generlate.com/media/generated_objects/" + data.generated_object_file_path;
           setModel("https://api.generlate.com/media/generated_objects/" + data.generated_object_file_path);
           setShowDownloadButton(true);
-
-          const newParagraph = document.createElement("p");
-          newParagraph.textContent = inputText;
-
-          const targetSection = document.querySelector(
-            "section:nth-of-type(2) div:first-of-type"
-          );
-
-          if (targetSection instanceof HTMLElement) {
-            targetSection.appendChild(newParagraph);
-          }
-
           input.value = "";
-
         })
         .catch((error) => {
           console.error("Error:", error);
-          // setModel("/box_4.obj");
+          // Handle errors here
         });
     }
   }
