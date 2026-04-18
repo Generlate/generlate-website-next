94c4f3a9ce7755c38cc97d441b4d2284f6be8524
Austen Cabret <austencabret@gmail.com>
2026-04-18 19:52:11 -0400

    rollback previous commit

--- src/app/(pages)/donate/page.tsx
+++ src/app/(pages)/donate/page.tsx
@@ -3,7 +3,6 @@
 import transition from '@/app/components/transition';
 import styles from "@/app/styles/donate.module.css";
 import Image from "next/image";
-import Script from 'next/script';
 
 declare module "react" {
   namespace JSX {
@@ -21,16 +20,15 @@ function Donate() {
              <p className={styles.p}>Your donation directly helps us build Generlate and post videos.</p>
         </div>
         <div className={styles.div}>
-            <Script async
-                src="https://js.stripe.com/v3/buy-button.js"
-                strategy="afterInteractive" 
-            />
+            <script async
+                src="https://js.stripe.com/v3/buy-button.js">
+            </script>
 
             <stripe-buy-button
-              className={styles.stripe}
-              buy-button-id="buy_btn_1PkKzFAYvBPxUj4WFNSNSHbq"
-              publishable-key="pk_live_51PkBqUAYvBPxUj4W2c1cu9HfxP15BeRx3ZgQp8XBwAXAzegzFGFzoXhDLQd06mzdWsn7Nz8g5FWQ7yQKpRHzGmqi00SSNfcqgW"
-            >
+        className={styles.stripe}
+  buy-button-id="buy_btn_1PkKzFAYvBPxUj4WFNSNSHbq"
+  publishable-key="pk_live_51PkBqUAYvBPxUj4W2c1cu9HfxP15BeRx3ZgQp8XBwAXAzegzFGFzoXhDLQd06mzdWsn7Nz8g5FWQ7yQKpRHzGmqi00SSNfcqgW"
+>
             </stripe-buy-button>
         </div>
    </main>
