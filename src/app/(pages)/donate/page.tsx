eeddd18d370d71f1255c57402336670b4bab4b0d
Austen Cabret <austencabret@gmail.com>
2026-04-18 20:27:42 -0400

    fix error: "can't access property "payload", t.message is undefined

--- src/app/(pages)/donate/page.tsx
+++ src/app/(pages)/donate/page.tsx
@@ -3,6 +3,8 @@
 import transition from '@/app/components/transition';
 import styles from "@/app/styles/donate.module.css";
 import Image from "next/image";
+import Script from 'next/script';
+import { useEffect, useState } from 'react';
 
 declare module "react" {
   namespace JSX {
@@ -13,25 +15,34 @@ declare module "react" {
 }
 
 function Donate() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
     <main className={styles.main}>
-        <div className={styles.div}>
-            <Image src="/austen-cabret-profile.webp" alt="Austen Cabret" width="200" height="200" className={styles.img}></Image> 
-             <p className={styles.p}>Your donation directly helps us build Generlate and post videos.</p>
-        </div>
-        <div className={styles.div}>
-            <script async
-                src="https://js.stripe.com/v3/buy-button.js">
-            </script>
+      <div className={styles.div}>
+        <Image src="/austen-cabret-profile.webp" alt="Austen Cabret" width="200" height="200" className={styles.img} />
+        <p className={styles.p}>Your donation directly helps us build Generlate and post videos.</p>
+      </div>
 
-            <stripe-buy-button
-        className={styles.stripe}
-  buy-button-id="buy_btn_1PkKzFAYvBPxUj4WFNSNSHbq"
-  publishable-key="pk_live_51PkBqUAYvBPxUj4W2c1cu9HfxP15BeRx3ZgQp8XBwAXAzegzFGFzoXhDLQd06mzdWsn7Nz8g5FWQ7yQKpRHzGmqi00SSNfcqgW"
->
-            </stripe-buy-button>
-        </div>
-   </main>
+      <div className={styles.div}>
+        <Script 
+          src="https://js.stripe.com/v3/buy-button.js" 
+          strategy="afterInteractive" 
+        />
+
+        {isMounted && (
+          <stripe-buy-button
+            className={styles.stripe}
+            buy-button-id="buy_btn_1PkKzFAYvBPxUj4WFNSNSHbq"
+            publishable-key="pk_live_51PkBqUAYvBPxUj4W2c1cu9HfxP15BeRx3ZgQp8XBwAXAzegzFGFzoXhDLQd06mzdWsn7Nz8g5FWQ7yQKpRHzGmqi00SSNfcqgW"
+          />
+        )}
+      </div>
+    </main>
   );
 }
 
